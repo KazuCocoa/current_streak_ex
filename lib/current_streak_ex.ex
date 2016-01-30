bae05d69b392fc1afb1d9b057582bcb1628e7fe1
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2016-01-30 22:05:19 +0900

    implement getting current streak and latest data

--- lib/current_streak_ex.ex
+++ lib/current_streak_ex.ex
@@ -1,2 +1,54 @@
 defmodule CurrentStreakEx do
+  alias Floki
+  alias HTTPoison
+
+  @spec contributions(binary) :: binary
+  def contributions(user) do
+    contribution_url = "https://github.com/users/#{user}/contributions"
+    HTTPoison.get!(contribution_url)
+    |> Map.get(:body)
+  end
+
+  @spec current_streak(binary) :: integer | nil
+  def current_streak(html) do
+    html
+    |> days
+    |> filter("data-count")
+    |> Enum.find_index(fn x -> x == 0 end)
+  end
+
+  @spec latest_streak_day(binary) :: binary | nil
+  def latest_streak_day(html) do
+    html
+    |> days
+    |> filter("data-date")
+    |> List.first
+  end
+
+  defp days(html), do: html |> Floki.find(".day")
+
+  defp filter(tuples, "data-count") do
+    tuples
+    |> Enum.reduce([], fn {_, day, _}, acc ->
+      Enum.into(acc, [Enum.filter(day, fn x ->
+        elem(x, 0) == "data-count"
+      end)
+      |> hd
+      |> elem(1)
+      |> String.to_integer
+      ])
+    end)
+  end
+
+  defp filter(tuples, "data-date") do
+    tuples
+    |> Enum.reduce([], fn {_, day, _}, acc ->
+      Enum.into(acc, [Enum.filter(day, fn x ->
+        elem(x, 0) == "data-date"
+      end)
+      |> hd
+      |> elem(1)
+      ])
+    end)
+  end
 end
--- test/current_streak_ex_test.exs
+++ test/current_streak_ex_test.exs
@@ -1,8 +1,26 @@
 defmodule CurrentStreakExTest do
   use ExUnit.Case
-  doctest CurrentStreakEx
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  alias CurrentStreakEx
+
+  setup do
+    html = File.read!("test/data/contributions.html")
+    {:ok, html: html}
+  end
+
+  test "#current_streak", context do
+    assert CurrentStreakEx.current_streak(context[:html]) == 4
+  end
+
+  test "#current_streak is nil" do
+    assert CurrentStreakEx.current_streak("fail case") == nil
+  end
+
+  test "#latest_streak_day", context do
+    assert CurrentStreakEx.latest_streak_day(context[:html]) == "2016-01-30"
+  end
+
+  test "#latest_streak_day is nil" do
+    assert CurrentStreakEx.latest_streak_day("fail case") == nil
   end
 end
